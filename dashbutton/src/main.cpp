e357dbf1c7acf56550dc8626c02adeb91d8f730a
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-06-25 11:59:34 +0200

    add basic display function
    Co-authored-by: Finn Nils Gedrath <fgedrath@th-koeln.de>

--- dashbutton/src/main.cpp
+++ dashbutton/src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
 #define PIN1 33
 #define PIN2 26
+
+const int color[] = {255, 0, 0};
 
 void setup() {
     pinMode(PIN1, INPUT);
     pinMode(PIN2, OUTPUT);
     Serial.begin(9600);
+    lcd.begin(16, 2);
+    lcd.setRGB(color[0], color[1], color[2]);
+    lcd.print("hello, iot!");
 }
 
 void loop() {
-    byte currentState = !digitalRead(PIN1);
+    byte currentState = digitalRead(PIN1);
 
     if (currentState == HIGH) {
         digitalWrite(PIN2, HIGH);
