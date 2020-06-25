17858c698a7acd3699038109ddb6bbf4fec7e764
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-06-25 14:15:40 +0200

    update states

--- dashbutton/src/main.cpp
+++ dashbutton/src/main.cpp
@@ -10,22 +10,23 @@
 #define WAITING_TO_ABORT 20
 #define ABORT 21
 #define SENDING 30
-#define RESPONSE 40
+#define RESET 40
 
 rgb_lcd lcd;
 byte isPressed = LOW;
 int currentState = WAITING_TO_START;
 bool firstTime = true;
+bool isClear = false;
+int countdown = 0;
 
 const int color[] = {255, 0, 136};
-
 
 void setup() {
     pinMode(PIN_BUTTON, INPUT);
     // Serial.begin(9600);
     lcd.begin(16, 2);
     lcd.setRGB(color[0], color[1], color[2]);
-    
+
     lcd.print("");
 }
 
@@ -48,15 +49,55 @@ void loop() {
             break;
 
         case AUTHENTICATION:
-            lcd.clear();
-            lcd.print("Halten Sie RFID an Sensor.");
+            // enter state
+            if (firstTime) {
+                lcd.clear();
+                lcd.print("Halten Sie RFID an Sensor.");  // TODO: umbruch
+
+                firstTime = false;
+                isClear = false;
+            }
+
+            // enter state
             isPressed = digitalRead(PIN_BUTTON);
-            if (isPressed == HIGH) {
+            if (!isClear && isPressed == LOW) {
+                isClear = true;
+            }
+            if (isClear && isPressed == HIGH) {
+                firstTime = true;
                 currentState = WAITING_TO_ABORT;
             }
             break;
 
         case WAITING_TO_ABORT:
+            // enter state
+            if (firstTime) {
+                lcd.clear();
+                lcd.print("Verarbeiten – Abbrechen?");  // TODO: umbruch
+
+                countdown = 3000;
+                firstTime = false;
+                isClear = false;
+            }
+
+            // go down
+            countdown--;
+
+            // check for button
+            isPressed = digitalRead(PIN_BUTTON);
+            if (!isClear && isPressed == LOW) {
+                isClear = true;
+            }
+            if (isClear && isPressed == HIGH) {
+                firstTime = true;
+                currentState = ABORT;
+            }
+
+            // normal leave
+            if (countdown == 0) {
+                firstTime = true;
+                currentState = SENDING;
+            }
             break;
 
         case ABORT:
@@ -65,13 +106,33 @@ void loop() {
             delay(100);
             lcd.clear();
             delay(100);
-            currentState = WAITING_TO_START;
+            currentState = RESET;
             break;
 
         case SENDING:
+            if (firstTime) {
+                lcd.clear();
+                lcd.print("Bestellung wurde gesendet");  // TODO: Umbruch
+
+                firstTime = false;
+            }
+
+            // MQTT Nachricht senden
+
+            // delay(100);
+            currentState = RESET;
             break;
 
-        case RESPONSE:
+        case RESET:  // vllt überspringen
+            if (firstTime) {
+                lcd.clear();
+                lcd.print("Danke!");  // TODO: Umbruch
+
+                firstTime = false;
+            }
+
+            // delay(100);
+            currentState = WAITING_TO_START;
             break;
     }
 }
