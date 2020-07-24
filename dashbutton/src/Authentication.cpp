10fd9e531721c73a722506ebe1c115df861f3120
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-07-24 17:08:30 +0200

    added rfid

---
+++ dashbutton/src/Authentication.cpp
@@ -0,0 +1,15 @@
+#include "Authentication.h"
+
+unsigned long auth_getUID(MFRC522 *mfrc522) {
+    if (!(*mfrc522).PICC_ReadCardSerial()) {
+        return -1;
+    }
+    unsigned long hex_num;
+    hex_num = (*mfrc522).uid.uidByte[0] << 24;
+    hex_num += (*mfrc522).uid.uidByte[1] << 16;
+    hex_num += (*mfrc522).uid.uidByte[2] << 8;
+    hex_num += (*mfrc522).uid.uidByte[3];
+    (*mfrc522).PICC_HaltA();
+
+    return hex_num;
+}
